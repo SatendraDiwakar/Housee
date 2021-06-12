e779f43f520118c07abfa170654a6c4dd4b2d0bb
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-06-13 00:01:35 +0530

    Build House and Houses component

--- src/Components/House.js
+++ src/Components/House.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
-export default function House({houseImage , houseType , rentPrice }){
-    return <div>
-        house
-    </div>
+
+export default function House({ houseImage, houseType, rentPrice, rating }) {
+
+
+
+    return (
+        <div className="card"
+            onMouseOver={() => {
+                document.getElementsByClassName("small-detail")[0].classList.remove("hide")
+                document.getElementsByClassName("house-detail")[0].classList.add("house-detail-hover")
+            }}
+            onMouseOut={() => {
+                document.getElementsByClassName("small-detail")[0].classList.add("hide")
+                document.getElementsByClassName("house-detail")[0].classList.remove("house-detail-hover")
+            }}
+        >
+
+            <div className="card-img">
+                <img src={houseImage} alt="House Image" />
+            </div>
+            <div className="house-detail">
+                <h1 className="card-type">{houseType}</h1>
+                <div className="small-detail hide">
+                    <div className="od">
+                        <p className="rating">Rating</p>
+                        <p>{" " + rating}</p>
+                    </div>
+                    <div className="od">
+                        <p className="price">Cost</p>
+                        <p>{rentPrice}<span className="pn">night</span></p>
+                    </div>
+                    <div className="od">
+                        <button className="features">Features</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/Pages/Houses.js
+++ src/Pages/Houses.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import House from '../Components/House'
 
 import houseHero from '../images/houses/houseHero.jpg'
+import house1 from '../images/houses/house-3.jpg'
 
-export default function Houses(){
+export default function Houses() {
 
     const heroHouse = {
         height: "70vh",
@@ -16,11 +18,16 @@ export default function Houses(){
             <Navbar housesStyle="house-nav" />
         </div>
         <div className="house-head">
-            <div className="house-heading">Find your relax</div>
+            <div className="house-heading">Find your place</div>
             <div className="line"></div>
         </div>
         <div className="houses">
-
+            <House
+                houseImage={house1}
+                houseType="Deluxe"
+                rentPrice="$600/"
+                rating="4.9"
+            />
         </div>
     </div>
 }
