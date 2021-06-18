691424018f79d3d88a14396ad00b3d92c06af399
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-06-18 12:55:14 +0530

    Responsive_V3

--- src/Components/House.js
+++ src/Components/House.js
@@ -1,26 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 
 export default function House({ houseImage, houseType, rentPrice, rating, itemNo }) {
 
     itemNo = parseInt(itemNo);
 
-    return (
-        <div className="card"
-            onMouseOver={() => {
-                document.getElementsByClassName("small-detail")[itemNo-1].classList.remove("hide")
-            }}
-            onMouseOut={() => {
-                document.getElementsByClassName("small-detail")[itemNo-1].classList.add("hide")
-            }}
-        >
+    function handleClick1(){
+        console.log("clicked");
+        document.getElementsByClassName("house-detail")[itemNo-1].classList.toggle("show")
+        document.getElementsByClassName("show1")[itemNo-1].classList.add("hide")
+        document.getElementsByClassName("show2")[itemNo-1].classList.remove("hide")
 
+    }
+    function handleClick2(){
+        console.log("clicked");
+        document.getElementsByClassName("house-detail")[itemNo-1].classList.toggle("show")
+        document.getElementsByClassName("show1")[itemNo-1].classList.remove("hide")
+        document.getElementsByClassName("show2")[itemNo-1].classList.add("hide")
+
+    }
+
+    return (
+        <div className="card">
             <div className="card-img">
                 <img src={houseImage} alt="HouseImage" />
             </div>
             <div className="house-detail">
+                <FaAngleUp 
+                    className="icon-button show1"
+                    onClick={handleClick1}
+                />
+                <FaAngleDown 
+                    className="icon-button show2 hide"
+                    onClick={handleClick2}
+                />
                 <h1 className="house-type">{houseType}</h1>
-                <div className="small-detail hide">
+                <div className="small-detail">
                     <div className="od">
                         <p className="rating">Rating</p>
                         <p>{" " + rating}</p>
