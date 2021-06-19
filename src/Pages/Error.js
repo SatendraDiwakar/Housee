248c2efa91e1068aea656a0509e1a2f8644a2f35
Satendra Kumar Diwakar <sadevexe@gmail.com>
2021-06-19 19:52:32 +0530

    Test load6

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { HouseContext } from '../Context'
 
-export default function Error(){
+export default function Error() {
+
+
+    const contextData = useContext(HouseContext);
+    const { housesHero } = contextData;
+
+    const heroHouse = {
+        height: "100vh",
+        width: "100%",
+        background: `url("${housesHero}") center no-repeat fixed`,
+        backgroundSize: "cover"
+    }
 
     return <>
-        Error
+        <div style={heroHouse} className="houses-cent">
+            <div className="houses-head" style={{width: "50rem"}}>
+                <p className="main-heading tagline">Error 404</p>
+                <div className="line"></div>
+                <p className="sub-heading">Return Back to Home</p>
+                <Link to="/" className="btn">Home</Link>
+            </div>
+        </div>
     </>
 }
